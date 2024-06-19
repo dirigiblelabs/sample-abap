933d4ce5116640e751c585257a80a5015adb5132
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-06-19 16:26:37 +0300

    cleanup

--- build-mac.sh
+++ build-mac.sh
@@ -5,4 +5,4 @@ npm run transpile
 npm run lint
 find . -name '*.mjs' -print0 | xargs -0 sed -i '' 's/%23/#/g'
 
-esbuild run.mjs --tsconfig=./tsconfig.json --bundle --outdir=dist --format=esm --target=es2022 --external:tls --external:net --external:util --external:util/types --external:crypto --external:zlib --external:http --external:https --external:fs --external:path --external:url --external:sdk  --external:kronos --inject:./polyfills/buffer.js --inject:./polyfills/process.js --out-extension:.js=.mjs
+esbuild run.mjs --tsconfig=./tsconfig.json --bundle --outdir=dist --format=esm --target=es2022 --external:tls --external:net --external:util --external:util/types --external:crypto --external:zlib --external:http --external:https --external:fs --external:path --external:url --external:sdk --inject:./polyfills/buffer.js --inject:./polyfills/process.js --out-extension:.js=.mjs
