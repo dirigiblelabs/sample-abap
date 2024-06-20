eceb6278ceca47c1af95f5cecbd6e3b289084dc1
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-06-20 12:15:44 +0300

    update build script

--- build-mac.sh
+++ build-mac.sh
@@ -1,8 +1,18 @@
-npm install
 rm -rf output
 rm -rf dist
+rm -rf node_modules
+
+npm install
+
 npm run transpile
+
 npm run lint
+
+# Find all .mjs files in the current directory and its subdirectories, 
+# and replaces all occurrences of %23 with # in those files.
 find . -name '*.mjs' -print0 | xargs -0 sed -i '' 's/%23/#/g'
 
-esbuild run.mjs --tsconfig=./tsconfig.json --bundle --outdir=dist --format=esm --target=es2022 --external:tls --external:net --external:util --external:util/types --external:crypto --external:zlib --external:http --external:https --external:fs --external:path --external:url --external:sdk --inject:./polyfills/buffer.js --inject:./polyfills/process.js --out-extension:.js=.mjs
+esbuild run.mjs --tsconfig=./tsconfig.json --bundle --outdir=dist --format=esm --target=es2022 \
+    --external:tls --external:net --external:util --external:crypto --external:zlib \
+    --external:http --external:https --external:fs --external:path --external:url --external:sdk \
+    --inject:./polyfills/buffer.js --inject:./polyfills/process.js --out-extension:.js=.mjs
